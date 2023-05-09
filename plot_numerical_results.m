59ef6ce1c72c13c523fdd263491b9b4fbc8ea4c0
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-05-09 23:03:58 +1000

    Refactor the MATLAB script

--- plot_numerical_results.m
+++ plot_numerical_results.m
@@ -11,13 +11,13 @@ low = data(5,:);
 figure;
 
 subplot(2,1,1);
-plot(tick,gini)
-xlabel("Time");
-ylabel("Gini Index");
-axis([0 600 0 1]);
-
-subplot(2,1,2);
 plot(tick, up, tick, mid, tick, low, "--");
 xlabel("Tick");
 ylabel("Number of People");
 legend("Up","Middle","Low");
+
+subplot(2,1,2);
+plot(tick,gini)
+xlabel("Time");
+ylabel("Gini Index");
+axis([0 800 0 1]);
