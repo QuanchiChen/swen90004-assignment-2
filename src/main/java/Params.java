c5c2cb1e47706dd1fa42e4683f2fdac0a6049293
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-05-09 20:04:54 +1000

    Change the maximum clock tick to 800

--- src/main/java/Params.java
+++ src/main/java/Params.java
@@ -6,7 +6,7 @@
  */
 public final class Params {
     // The maximum clock tick
-    public static final int MAX_TICK = 600;
+    public static final int MAX_TICK = 800;
     // The maximum horizontal and vertical coordinate
     // Since the minimal coordinate is 0, the grid consists of 33 x 33 patches.
     public static final int MAX_COORDINATE = 32;
